26b5a5f181fd0182a31ce1b49cb9785162e8ee6e
Johannes Langer <Johannes.K.Langer@campus.lmu.de>
2017-08-30 18:28:05 +0200

    tSNE mit Wahl-O-Mat btw 2017

---
+++ README.md
@@ -0,0 +1,12 @@
+# t-SNE deutscher Parteien basierend auf deren Antworten zum Wahl-O-Mat der Bundestagswahl 2017.
+
+32 Parteien beantworteten 38 Fragen zum Wahl-O-Mat der Bundestagswahl 2017. Diese wurden eingelesen und folgendermaßen kodiert:
+- nein: -1
+- neutral: 0
+- ja: 1
+
+Das "t-distributed stochastic neighbor embedding" - Verfahren wird verwendet um hochdimensionale Datensätze zweidimensional darzustellen. Die 38 Fragen werden so auf 2 Dimensionen reduziert, wobei möglichst viel der statistischen Ähnlichkeit zwischen den ursprünglichen Daten erhalten bleiben soll.
+
+## Quellen
+- *Wahl-O-Mat*: https://www.wahl-o-mat.de/bundestagswahl2017/PositionsvergleichBundestagswahl2017
+- *Rtsne*: https://cran.r-project.org/web/packages/Rtsne/
---
+++ wahlomat.R
@@ -0,0 +1,78 @@
+# UTF-8 Format für Umlaute
+Sys.setlocale("LC_ALL", "German")
+
+# Quelle: https://www.wahl-o-mat.de/bundestagswahl2017/PositionsvergleichBundestagswahl2017
+# einlesen der Antworten der Partein auf die Wahl-O-Mat Fragen 2017
+wahlomat = read.csv("wahlomat.csv")
+
+# nein = 0 -> -1
+wahlomat[wahlomat==0] = -1
+
+# neutral = _ -> 0
+wahlomat[is.na(wahlomat)] = 0
+
+# volle Namen der Partein
+parteinamen = c(
+  "CDU/CSU",
+  "SPD",
+  "DIE LINKE",
+  "GRÜNE",
+  "FDP",
+  "AfD",
+  "PIRATEN",
+  "NPD",
+  "FREIE WÄHLER",
+  "Tierschutzpartei",
+  "ÖDP",
+  "Die PARTEI",
+  "BP",
+  "Volksabstimmung",
+  "PDV",
+  "MLPD",
+  "BüSo",
+  "SGP",
+  "DIE RECHTE",
+  "Allianz Deutscher Demokraten",
+  "Tierschutzallianz",
+  "B*",
+  "BGE",
+  "DiB",
+  "DKP",
+  "DM",
+  "Die Grauen",
+  "du.",
+  "MENSCHLICHE WELT",
+  "Die Humanisten",
+  "Gesundheitsforschung",
+  "V-Partei³"
+)
+
+# Quelle: https://cran.r-project.org/web/packages/Rtsne/
+# https://cran.r-project.org/web/packages/Rtsne/README.html
+# tSNE embedding mit perplexity von 5 und default seed von 42
+library(Rtsne)
+set.seed(42) 
+tsne_out <- Rtsne(t(as.matrix(wahlomat)), perplexity=5)
+
+# plot der Partein
+pdf("parteien_tsne.pdf", 14, 10)
+
+# anpassen der Ränder und Überschirft
+par(mar = c(2,2,2,2), oma = c(2,2,2,2))
+mins = apply(tsne_out$Y,2,min) * 1.1
+maxs = apply(tsne_out$Y,2,max) * 1.1
+plot(c(mins[1], maxs[1]), c(mins[2], maxs[2]), type="n", ylab='', xlab='', axes=FALSE, cex.main=1.3, font=2,
+     main= "tSNE deutscher Parteien basierend auf Wahl-O-Mat Fragen zur Bundestagswahl 2017")
+
+# plotten der tsne Werte
+points(tsne_out$Y, col="steelblue2", pch=15)
+
+# plotten der Parteinamen
+text(tsne_out$Y, labels=parteinamen, pos = 1, cex= 1.2, col="gray30", font=2)
+
+# Gitter
+grid(10, lwd=2)
+
+# ursprüngliches git Projekt
+text(mins[1] * 0.8, mins[2], "https://github.com/jklanger/btw2017", font=3, cex=0.75)
+dev.off()
